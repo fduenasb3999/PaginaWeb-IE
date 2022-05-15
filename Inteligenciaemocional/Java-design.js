8350f64146aca11bd90a68d675ac4bb2819646d6
fduenasb3999 <104692386+fduenasb3999@users.noreply.github.com>
2022-05-14 21:32:14 -0500

    Proyecto finalizado, estructura mejorada

---
+++ Inteligenciaemocional/Java-design.js
@@ -0,0 +1,373 @@
+var questionBank= [
+    {
+        question : 'Sé cuando hablar de mis problemas personales con otras personas.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'C'
+    },
+    {
+        question : 'Cuando me encuentro con obstáculos, recuerdo las veces en que me enfrenté y superé situaciones similares.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'D'
+    },
+    {
+        question : 'Creo que me irá bien en la mayoría de cosas que intento.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'D'
+    },
+    {
+        question : 'Las personas confían fácilmente en mi.',
+        option : ['Les cuesta trabajo','Lo hacen con facilidad'],
+        answer : 'Lo hacen con facilidad',
+        type: 'E'
+    },
+    {
+        question : 'Me parece complejo entender mensajes no verbales de otras personas. (Gestos, miradas, posturas corporales o mensajes no explícitos) ',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'A'
+    },
+    {
+        question : 'Algunos de los eventos más trascendentales en mi vida me han permitido re-evaluar qué es importante y qué no.',
+        option : ['Me lo han permitido','No me lo han permitido'],
+        answer : 'Me lo han permitido',
+        type: 'B'
+    },
+    {
+        question : 'Cuando cambia mi estado de ánimo, veo nuevas posibilidades. ',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'B'
+    },
+    {
+        question : 'Considero que las emociones son una de las cosas que hacen que vivir valga la pena. ',
+        option : ['De acuerdo','En desacuerdo'],
+        answer : 'De acuerdo',
+        type: 'B'
+    },
+    {
+        question : 'Soy consciente de mis emociones a medida que las experimento.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'A'
+    },
+    {
+        question : 'Espero que sucedan cosas buenas.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'D'
+    },
+    {
+        question : 'Me gusta compartir mis emociones con otros.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'E'
+    },
+    {
+        question : 'Cuando experimento una emoción positiva, sé como hacerla durar.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'C'
+    },
+    {
+        question : 'Organizo eventos para que otros disfruten.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'E'
+    },
+    {
+        question : 'Busco realizar actividades que me hacen feliz.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'D'
+    },
+    {
+        question : 'Soy consciente de los mensajes de tipo no verbal que le transmito a otras personas. ',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'A'
+    },
+    {
+        question : 'Me presento a otros tratando de causar una buena impresión de forma amable.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'E'
+    },
+    {
+        question : 'Resuelvo problemas fácilmente cuando mi estado de ánimo es positivo.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'B'
+    },
+    {
+        question : 'Reconozco las emociones que experimentan otras personas al ver sus expresiones faciales. ',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'A'
+    },
+    {
+        question : 'Sé por qué cambian mis emociones.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'A'
+    },
+    {
+        question : 'Cuando mi estado de ánimo es positivo, soy capaz de proponer nuevas ideas.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'B'
+    },
+    {
+        question : 'Tengo el control sobre mis emociones.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'C'
+    },
+    {
+        question : 'Reconozco fácilmente mis emociones cuando las experimento.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'A'
+    },
+    {
+        question : 'Me motivo imaginando un buen resultado en las tareas que asumo.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'D'
+    },
+    {
+        question : 'Felicito a los demás cuando realizan un buen trabajo.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'E'
+    },
+    {
+        question : 'Soy consciente de los mensajes no verbales que envían otras personas.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'A'
+    },
+    {
+        question : 'Cuando una persona me habla de un evento importante en su vida, casi siento como si yo mismo hubiera experimentado este evento.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'E'
+    },
+    {
+        question : 'Frecuentemente propongo nuevas ideas después de sentir un cambio en mis emociones.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'B'
+    },
+    {
+        question : 'Al enfrentar un reto, tiendo a rendirme porque creo que fallaré.',
+        option : ['No me rindo facilmente','Lo hago con facilidad'],
+        answer : 'No me rindo facilmente',
+        type: 'D'
+    },
+    {
+        question : 'Sé lo que otras personas sienten con tan solo mirarlas.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'A'
+    },
+    {
+        question : 'Ayudo a otras personas a sentirse mejor cuando están tristes o deprimidas.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'E'
+    },
+    {
+        question : 'Uso mi buena actitud como motivación para esforzarme y superar obstáculos.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'D'
+    },
+    {
+        question : 'Puedo saber cómo se sienten las personas al escuchar su tono de voz.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'A'
+    },
+    {
+        question : 'Tengo dificultad en entender por qué las personas se sienten de determinada manera.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'A'
+    },
+    {
+        question : 'Puedo controlar mi temperamento y manejar mis dificultades racionalmente',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'C'
+    },
+    {
+        question : 'Siempre puedo calmarme rápidamente cuando estoy enojado.',
+        option : ['Me cuesta trabajo','Lo hago con facilidad'],
+        answer : 'Lo hago con facilidad',
+        type: 'C'
+    }
+]
+
+var question= document.getElementById('question');
+var quizContainer= document.getElementById('quiz-container');
+var scorecard= document.getElementById('scorecard');
+var option0= document.getElementById('option0');
+var option1= document.getElementById('option1');
+var next= document.querySelector('.next');
+var points= document.getElementById('score');
+var span= document.querySelectorAll('span');
+var i=0;
+var scoreA= 0;
+var scoreB= 0;
+var scoreC= 0;
+var scoreD= 0;
+var scoreE= 0;
+
+//function to display questions
+function displayQuestion(){
+    for(var a=0;a<span.length;a++){
+        span[a].style.background='none';
+    }
+    question.innerHTML= (i+1)+'. '+questionBank[i].question;
+    option0.innerHTML= questionBank[i].option[0];
+    option1.innerHTML= questionBank[i].option[1];
+    
+    stat.innerHTML= "Pregunta"+' '+(i+1)+' '+'de'+' '+questionBank.length;
+}
+
+//function to calculate scores
+
+function calcScore(e){
+if(questionBank[i].type==="A"){
+    if(e.innerHTML===questionBank[i].answer)
+    {
+        scoreA= scoreA+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else{
+        document.getElementById(e.id).style.background= 'tomato';
+    }
+    setTimeout(nextQuestion,300);
+} 
+if(questionBank[i].type==="B"){
+    if(e.innerHTML===questionBank[i].answer)
+    {
+        scoreB= scoreB+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else{
+        document.getElementById(e.id).style.background= 'tomato';
+    }
+    setTimeout(nextQuestion,300);
+}
+if(questionBank[i].type==="C"){
+    if(e.innerHTML===questionBank[i].answer)
+    {
+        scoreC= scoreC+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else{
+        document.getElementById(e.id).style.background= 'tomato';
+    }
+    setTimeout(nextQuestion,300);
+}
+if(questionBank[i].type==="D"){
+    if(e.innerHTML===questionBank[i].answer)
+    {
+        scoreD= scoreD+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else{
+        document.getElementById(e.id).style.background= 'tomato';
+    }
+    setTimeout(nextQuestion,300);
+}
+if(questionBank[i].type==="E"){
+    if(e.innerHTML===questionBank[i].answer)
+    {
+        scoreE= scoreE+1;
+        document.getElementById(e.id).style.background= 'limegreen';
+    }
+    else{
+        document.getElementById(e.id).style.background= 'tomato';
+    }
+    setTimeout(nextQuestion,300);
+}
+}
+
+
+//function to display next question
+function nextQuestion(){
+    if(i<questionBank.length-1)
+    {
+        i=i+1;
+        displayQuestion();
+    }
+    else{
+        
+        quizContainer.style.display= 'none';
+        scoreboard.style.display= 'block'
+    }
+}
+
+//click events to next button
+next.addEventListener('click',nextQuestion);
+
+//Back to Quiz button event
+function backToQuiz(){
+    location.reload();
+}
+
+
+//x, y axis 
+var xValues = ["PEE", "UEFP", "RE", "GEP", "GEOP"];
+var yValues = [1, 1, 1, 1, 1];
+
+//function to check Answers
+function checkAnswer(){
+    answerBank.style.display= 'block';
+    scoreboard.style.display= 'none';
+    scoreB=scoreB*10/6;
+    scoreC=scoreC*10/4;
+    scoreD=scoreD*10/7;
+    scoreE=scoreE*10/7;
+    
+    yValues[0] = scoreA.toFixed()
+    yValues[1] = scoreB.toFixed()
+    yValues[2] = scoreC.toFixed()
+    yValues[3] = scoreD.toFixed()
+    yValues[4] = scoreE.toFixed()
+
+    document.getElementById('scoreA').innerHTML = scoreA.toFixed();
+    document.getElementById('scoreB').innerHTML = scoreB.toFixed();
+    document.getElementById('scoreC').innerHTML = scoreC.toFixed();
+    document.getElementById('scoreD').innerHTML = scoreD.toFixed();
+    document.getElementById('scoreE').innerHTML = scoreE.toFixed();
+}
+
+
+displayQuestion();
+
+//chart js
+var barColors = ["red", "green","blue","orange","brown"];
+
+new Chart("myChart", {
+  type: "bar",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    title: {
+      display: true,
+      text: "Resultados Cuestionario Inteligencia Emocional"
+    }
+  }
+});
